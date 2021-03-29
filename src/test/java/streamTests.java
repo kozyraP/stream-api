b14d1fe9130294e34ed28fbd552da080dd2c0346
pawel <kozyra.mail@gmail.com>
2021-03-29 21:16:22 +0200

    Sort ops (reversed order)

--- src/test/java/streamTests.java
+++ src/test/java/streamTests.java
@@ -1,10 +1,7 @@
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class streamTests {
@@ -52,6 +49,13 @@ public class streamTests {
     public void filterOperation() {
         employees.stream()
                 .filter(employee -> employee.getFirstName().startsWith("J"))
+                .forEach(System.out::println);
+    }
+
+    @Test
+    public void sortOperation() {
+        employees.stream()
+                .sorted(Comparator.comparing(Employee::getAge).reversed())
                 .forEach(System.out::println);
     }
 }
