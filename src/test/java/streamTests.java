7433d11a77d35fb1fd8412985a672e892686ce93
pawel <kozyra.mail@gmail.com>
2021-03-29 16:41:59 +0200

    [useless] changing names in employees list

--- src/test/java/streamTests.java
+++ src/test/java/streamTests.java
@@ -14,9 +14,9 @@ public class streamTests {
     public void setUp() {
         employees.add(new Employee("Jan", "Kowalski", 29, Arrays.asList("Java", "Python")));
         employees.add(new Employee("Janina", "Udemy", 25, Arrays.asList("Java Script", "HTML")));
-        employees.add(new Employee("Jan", "Kowalski", 39, Arrays.asList("PHP", "Ruby")));
-        employees.add(new Employee("Jan", "Kowalski", 50, Arrays.asList("Java", "Scala")));
-        employees.add(new Employee("Jan", "Kowalski", 45, Arrays.asList("C", "C++", "Python")));
+        employees.add(new Employee("Janusz", "Amebowski", 39, Arrays.asList("PHP", "Ruby")));
+        employees.add(new Employee("Jarek", "Kozera", 50, Arrays.asList("Java", "Scala")));
+        employees.add(new Employee("Kajetan", "Masłowski", 45, Arrays.asList("C", "C++", "Python")));
     }
 
     @Test
