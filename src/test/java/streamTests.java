2f5b5f87214d90610b3c1d2444e16d414e0e89c2
pawel <kozyra.mail@gmail.com>
2021-03-29 16:31:17 +0200

    First commit after fixing gitignore file

---
+++ src/test/java/streamTests.java
@@ -0,0 +1,27 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class streamTests {
+
+
+    List<Employee> employees = new ArrayList<>();
+
+    @Before
+    public void setUp() {
+        employees.add(new Employee("Jan", "Kowalski", 29, Arrays.asList("Java", "Python")));
+        employees.add(new Employee("Janina", "Udemy", 25, Arrays.asList("Java Script", "HTML")));
+        employees.add(new Employee("Jan", "Kowalski", 39, Arrays.asList("PHP", "Ruby")));
+        employees.add(new Employee("Jan", "Kowalski", 50, Arrays.asList("Java", "Scala")));
+        employees.add(new Employee("Jan", "Kowalski", 45, Arrays.asList("C", "C++", "Python")));
+    }
+
+    @Test
+    public void testPrint(){
+        employees.stream()
+                .forEach(employee -> System.out.println(employee));
+    }
+}
